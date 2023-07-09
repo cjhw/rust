b0f56da05748684143564eeeb853fb365ad1261c
cjhw <1784581164@qq.com>
2023-07-10 01:12:07 +0800

    feat: kv-server one stage

--- kv/src/lib.rs
+++ kv/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod error;
+mod pb;
+mod service;
+mod storage;
+pub use error::KvError;
+pub use pb::abi::*;
+pub use service::*;
+pub use storage::*;
